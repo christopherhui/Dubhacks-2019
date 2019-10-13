37946c670c30b22ad171b5e3b30250bd0f6e03a8
Christopher Hui <christophergkhui@gmail.com>
2019-10-13 01:11:11 -0700

    maps added points

--- public/customize.js
+++ public/customize.js
@@ -1,85 +1,52 @@
-var map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: 40.674, lng: -73.945 },
-    zoom: 12,
-    styles: [
-        { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
-        { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
-        { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
-        {
-            featureType: 'administrative.locality',
-            elementType: 'labels.text.fill',
-            stylers: [{ color: '#d59563' }]
-        },
-        {
-            featureType: 'poi',
-            elementType: 'labels.text.fill',
-            stylers: [{ color: '#d59563' }]
-        },
-        {
-            featureType: 'poi.park',
-            elementType: 'geometry',
-            stylers: [{ color: '#263c3f' }]
-        },
-        {
-            featureType: 'poi.park',
-            elementType: 'labels.text.fill',
-            stylers: [{ color: '#6b9a76' }]
-        },
-        {
-            featureType: 'road',
-            elementType: 'geometry',
-            stylers: [{ color: '#38414e' }]
-        },
-        {
-            featureType: 'road',
-            elementType: 'geometry.stroke',
-            stylers: [{ color: '#212a37' }]
-        },
-        {
-            featureType: 'road',
-            elementType: 'labels.text.fill',
-            stylers: [{ color: '#9ca5b3' }]
-        },
-        {
-            featureType: 'road.highway',
-            elementType: 'geometry',
-            stylers: [{ color: '#746855' }]
-        },
-        {
-            featureType: 'road.highway',
-            elementType: 'geometry.stroke',
-            stylers: [{ color: '#1f2835' }]
-        },
-        {
-            featureType: 'road.highway',
-            elementType: 'labels.text.fill',
-            stylers: [{ color: '#f3d19c' }]
-        },
-        {
-            featureType: 'transit',
-            elementType: 'geometry',
-            stylers: [{ color: '#2f3948' }]
-        },
-        {
-            featureType: 'transit.station',
-            elementType: 'labels.text.fill',
-            stylers: [{ color: '#d59563' }]
-        },
-        {
-            featureType: 'water',
-            elementType: 'geometry',
-            stylers: [{ color: '#17263c' }]
-        },
-        {
-            featureType: 'water',
-            elementType: 'labels.text.fill',
-            stylers: [{ color: '#515c6d' }]
-        },
-        {
-            featureType: 'water',
-            elementType: 'labels.text.stroke',
-            stylers: [{ color: '#17263c' }]
-        }
-    ]
-});
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 40.674, lng: -73.945 },
+        zoom: 12,
+    });
 
+    // You can change the iconbase
+    var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+    var icons = {
+        occupied: {
+            name: 'Occupied',
+            icon: iconBase + 'parking_lot_maps.png'
+        },
+        unoccupied: {
+            name: 'Unoccupied',
+            icon: iconBase + 'library_maps.png'
+        },
+    };
+
+    // The purpose for this is to determine where the points are.
+    var features = [];
+    addPoint(40.674, -73.945);
+    regeneratePoints();
+
+    function addPoint(position1, position2, taken = false) {
+        if (taken) {
+            features.push({
+                position: new google.maps.LatLng(position1, position2),
+                type: 'occupied'
+            })
+        } else {
+            features.push({
+                position: new google.maps.LatLng(position1, position2),
+                type: 'unoccupied'
+            })
+        }
+    }
+
+    function removePoint(marker) {
+        marker.setMap(null)
+    }
+
+    function regeneratePoints() {
+        features.forEach(function (feature) {
+            var marker = new google.maps.Marker({
+                position: feature.position,
+                icon: icons[feature.type].icon,
+                map: map
+            });
+        });
+    }
+}
